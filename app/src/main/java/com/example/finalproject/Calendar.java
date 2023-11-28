ce1972c4affc5cfd2bfa66c8dbad4f1a346100fd
haileycummings333 <haileycummings333@gmail.com>
2023-11-28 13:03:29 -0600

    update4 11/28

--- app/src/main/java/com/example/finalproject/Calendar.java
+++ app/src/main/java/com/example/finalproject/Calendar.java
@@ -1,13 +1,17 @@
 package com.example.finalproject;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Calendar extends AppCompatActivity {
-
+    View cal;
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.calendarscreen);
+
+        cal = findViewById(R.id.calendar);
+
     }
 }
