aeaaeb0886be59049ed419b630def8331fa8bbc2
elisabethhassan <elisabethh.info@gmail.com>
2023-11-30 13:17:35 -0600

    added login & sign up w/ firebase

--- app/src/main/java/com/example/finalproject/Login.java
+++ app/src/main/java/com/example/finalproject/Login.java
@@ -1,18 +1,39 @@
 package com.example.finalproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Login extends AppCompatActivity {
     EditText email, password;
     Button loginButton;
+    FirebaseAuth firebaseAuth;
+    FirebaseUser firebaseUser;
+    TextView signup;
 
-    View.OnClickListener loginListenter = new View.OnClickListener() {
+    View.OnClickListener signUPListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(Login.this, Signup.class );
+            startActivity(intent);
+        }
+    };
+
+    View.OnClickListener loginListener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
             Intent intent = new Intent(Login.this, Options.class );
@@ -28,10 +49,38 @@ public class Login extends AppCompatActivity {
         email = findViewById(R.id.email_ET);
         password = findViewById(R.id.password_ET);
         loginButton = findViewById(R.id.button);
+        signup = findViewById(R.id.loginTV);
 
-        loginButton.setOnClickListener(loginListenter);
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        loginButton.setOnClickListener(loginListener);
+        signup.setOnClickListener(signUPListener);
     }
 
+    private void loginUser(){
+        String userEmail = email.getText().toString();
+        String userPassword = password.getText().toString();
 
+        // Validations for input email and password
+        if (TextUtils.isEmpty(userEmail)) {
+            Toast.makeText(getApplicationContext(), "Please enter email!!", Toast.LENGTH_LONG).show();
+            return;
+        }
+        if (TextUtils.isEmpty(userPassword)) {
+            Toast.makeText(getApplicationContext(), "Please enter password!!", Toast.LENGTH_LONG).show();
+            return;
+        }
 
+        firebaseAuth.signInWithEmailAndPassword(userEmail, userPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Toast.makeText(getApplicationContext(), "Login Successful!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Login Failed! Please try again.", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+    }
 }
--- app/src/main/java/com/example/finalproject/Signup.java
+++ app/src/main/java/com/example/finalproject/Signup.java
@@ -2,9 +2,11 @@ package com.example.finalproject;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,7 +22,10 @@ import com.google.firebase.auth.FirebaseAuthException;
 public class Signup extends AppCompatActivity {
     EditText fullName, email, password, confirmPassword;
     Button signUpButton, loginButton;
+
     FirebaseAuth firebaseAuth;
+    FirebaseUser firebaseUser;
+
     Boolean success;
 
 
@@ -33,6 +38,7 @@ public class Signup extends AppCompatActivity {
        confirmPassword = findViewById(R.id.confirmPasswordET);
 
        firebaseAuth = FirebaseAuth.getInstance();
+       firebaseUser = firebaseAuth.getCurrentUser();
 
        signUpButton = findViewById(R.id.signUpSubmit);
        loginButton = findViewById(R.id.loginButton);
@@ -45,7 +51,8 @@ public class Signup extends AppCompatActivity {
                if(passCheck)
                    success = confirmPasswords();
                if(success){
-                   //
+                   //add info to database
+                   onSignup();
                }
 
            }
@@ -102,13 +109,28 @@ public class Signup extends AppCompatActivity {
    }
 
 
-   public void onSignup(View view){
+   public void onSignup(){
        String userEmail = email.getText().toString();
        String userPassword = password.getText().toString();
+
+       // Validations for input email and password
+       if (TextUtils.isEmpty(userEmail)) {
+           Toast.makeText(getApplicationContext(), "Please enter email!!", Toast.LENGTH_LONG).show();
+           return;
+       }
+       if (TextUtils.isEmpty(userPassword)) {
+           Toast.makeText(getApplicationContext(), "Please enter password!!", Toast.LENGTH_LONG).show();
+           return;
+       }
+
        firebaseAuth.createUserWithEmailAndPassword(userEmail, userPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
            @Override
            public void onComplete(@NonNull Task<AuthResult> task) {
-
+                if (task.isSuccessful()){
+                    Toast.makeText(getApplicationContext(), "Registration Successful!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Registration Failed! Please try again.", Toast.LENGTH_LONG).show();
+                }
            }
        });
 
